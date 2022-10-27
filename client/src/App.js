2bf5aededa9c7efb1adb8895c2d16ecd87131e47
Gabe Vee <0gabevee0@gmail.com>
2022-10-27 14:47:33 -0600

    navbar appears on implemented pages

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Navbar";
 import Signup from "./pages/SignUp";
 import Login from "./pages/Login";
 
@@ -36,6 +37,8 @@ function App() {
     <Router>
       <ApolloProvider client={client}>
         <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <br />
           <div className="container">
             <Routes>
               <Route path="/signup" element={<Signup />} />
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Auth from "../../utils/auth";
+
+const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
