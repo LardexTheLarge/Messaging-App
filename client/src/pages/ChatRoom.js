b746251c2dda4e8dd275b13e2c1343bc77a86ed9
Gabe Vee <0gabevee0@gmail.com>
2022-11-02 19:39:11 -0600

    messages show on chat room

--- client/src/pages/ChatRoom.js
+++ client/src/pages/ChatRoom.js
@@ -3,23 +3,56 @@ import { useParams } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_SINGLE_CHATROOM } from "../utils/queries";
 import { POST_MESSAGE_TO_CHATROOM } from "../utils/mutations";
+import { GET_CHATROOM_MESSAGES } from "../utils/queries";
+import auth from "../utils/auth";
 
 const ChatRoom = () => {
   const { roomId } = useParams();
-
   const { loading, data } = useQuery(GET_SINGLE_CHATROOM, {
     variables: { roomId: roomId },
   });
-
   const chatRoom = data?.chatRoom || {};
 
+  function MessageList() {
+    const currentUser = auth.getUser().data.username;
+    const { loading, data, startPolling } = useQuery(GET_CHATROOM_MESSAGES, {
+      variables: { roomId },
+    });
+
+    const chatMessages = data?.chatRoomMessages.messages || [];
+
+    if (loading) {
+      return <p>loading...</p>;
+    } else {
+      return chatMessages?.map((message) => {
+        if (message.username === currentUser) {
+          return (
+            <li key={message._id}>
+              <div>{message.messageText}</div>
+            </li>
+          );
+        } else {
+          return (
+            <li key={message._id}>
+              <div>
+                <span>{message.username}</span>
+                <div>{message.messageText}</div>
+              </div>
+            </li>
+          );
+        }
+      });
+    }
+  }
+
+  //Message posting function
   function MessageEditor() {
     const [post] = useMutation(POST_MESSAGE_TO_CHATROOM);
     const [value, setValue] = useState("");
     function handleChange(e) {
       setValue(e.target.value);
     }
-    console.log(roomId);
+
     async function handleSubmit(e) {
       e.preventDefault();
       await send(value);
@@ -28,17 +61,28 @@ const ChatRoom = () => {
     async function send(messageText) {
       await post({ variables: { roomId, messageText } });
     }
+
     return (
-      <form id="form" action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={value}
-          onChange={handleChange}
-          id="input"
-          autoComplete="off"
-        />
-        <button>send</button>
-      </form>
+      <div className="p-3 bg-main fixed-bottom">
+        <form
+          id="form"
+          action=""
+          className="input-group"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="form-control"
+            type="text"
+            value={value}
+            onChange={handleChange}
+            id="input"
+            autoComplete="off"
+          />
+          <button className="bg-darkMain btn btn-outline-dark text-light">
+            send
+          </button>
+        </form>
+      </div>
     );
   }
 
@@ -47,7 +91,7 @@ const ChatRoom = () => {
   }
   return (
     <div>
-      <h2>{chatRoom.roomName}</h2>
+      <MessageList />
       <MessageEditor />
     </div>
   );
